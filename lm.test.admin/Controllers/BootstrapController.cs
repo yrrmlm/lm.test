511eba377bb2b499746ef77446747a3cb735a077
liumin <yrrmlm@163.com>
2019-02-16 20:54:03 +0800

    this project is just for pratice of myself

---
+++ lm.test.admin/Controllers/BootstrapController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lm.test.admin.Controllers
+{
+    public class BootstrapController : Controller
+    {
+        public PartialViewResult Dropdown()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult ButtonGroup()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ lm.test.admin/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace lm.test.admin.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
